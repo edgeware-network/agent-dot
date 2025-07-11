60bb566d870b43c10d60b87eca6b503a2e6fb8a5
pranavcpawar <pranavcpawar@outlook.com>
2025-07-12 01:18:21 +0530

    feat(setup): configure eslint and prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,40 @@
-import { FlatCompat } from "@eslint/eslintrc";
+// @ts-check
 
-const compat = new FlatCompat({
-	// import.meta.dirname is available after Node.js v20.11.0
-	baseDirectory: import.meta.dirname,
-	recommendedConfig: js.configs.recommended,
-	allConfigs: js.configs.all,
+import js from "@eslint/js";
+import prettierConfig from "eslint-config-prettier";
+import prettierPlugin from "eslint-plugin-prettier";
+import tseslint from "typescript-eslint";
+
+const IS_PRODUCTION = process.env.NODE_ENV === "production";
+
+export default tseslint.config({
+  languageOptions: {
+    parserOptions: {
+      ecmaVersion: 2022,
+      project: "./tsconfig.json",
+      projectService: true,
+      tsconfigRootDir: import.meta.dirname,
+    },
+    parser: tseslint.parser,
+  },
+  plugins: {
+    "@typescript-eslint": tseslint.plugin,
+    prettier: prettierPlugin,
+  },
+  ignores: ["next.config.js", "postcss.config.js", "public/**", ".next/**"],
+  files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+  rules: {
+    "@typescript-eslint/no-unused-vars": IS_PRODUCTION ? "error" : "warn",
+    "no-console": IS_PRODUCTION ? "error" : "warn",
+    "prettier/prettier": ["error"],
+    semi: ["error", "always"],
+    quotes: ["error", "double"],
+  },
+  extends: [
+    js.configs.recommended,
+    tseslint.configs.strict,
+    tseslint.configs.strictTypeChecked,
+    tseslint.configs.stylisticTypeChecked,
+    prettierConfig,
+  ],
 });
-
-const eslintConfig = [
-	...compat.extends("next/core-web-vitals", "next/typescript"),
-];
-
-export default eslintConfig;
