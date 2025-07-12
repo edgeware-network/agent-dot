f5c468314c869874f9e8f0a068de00357abdfadc
pranavcpawar <pranavcpawar@outlook.com>
2025-07-12 22:36:35 +0530

    chore(setup): add shadcn

---
+++ lib/font.ts
@@ -0,0 +1,53 @@
+import {
+  Geist,
+  Manrope,
+  Montserrat,
+  Poppins,
+  Unbounded,
+  Work_Sans,
+  Outfit,
+} from "next/font/google";
+
+const manrope = Manrope({
+  subsets: ["latin"],
+  variable: "--font-manrope",
+  weight: ["400", "500", "600", "700", "800"],
+});
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  variable: "--font-poppins",
+  weight: ["400", "500", "600", "700", "800", "900"],
+});
+
+const workSans = Work_Sans({
+  subsets: ["latin"],
+  variable: "--font-work-sans",
+  weight: ["400", "500", "600", "700", "800", "900"],
+});
+
+const unbounded = Unbounded({
+  subsets: ["latin"],
+  variable: "--font-unbounded",
+  weight: ["400", "500", "600", "700", "800", "900"],
+});
+
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  variable: "--font-montserrat",
+  weight: ["400", "500", "600", "700", "800", "900"],
+});
+
+const geist = Geist({
+  subsets: ["latin"],
+  variable: "--font-geist-sans",
+  weight: ["400", "500", "600", "700", "800", "900"],
+});
+
+const outfit = Outfit({
+  subsets: ["latin"],
+  variable: "--font-outfit",
+  weight: ["400", "500", "600", "700", "800", "900"],
+});
+
+export { geist, manrope, montserrat, poppins, unbounded, workSans, outfit };
