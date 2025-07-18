3dad7e1d685b455e3eebcee323881850056b2904
pranavcpawar <pranavcpawar@outlook.com>
2025-07-18 16:52:19 +0530

    feat(utils): add helper functions

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,8 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function trimAddress(address: string, length?: number) {
+  length ??= 4;
+  return `${address.slice(0, length)}...${address.slice(-length)}`;
+}
