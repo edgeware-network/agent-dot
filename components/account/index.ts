82e61c07972bb2d1ae919ad5c048d74b9a07b2f5
pranavcpawar <pranavcpawar@outlook.com>
2025-07-20 20:07:40 +0530

    feat(ui): implement multi-chain select btn

--- components/account/index.ts
+++ components/account/index.ts
@@ -1,2 +1,3 @@
+export { default as ChainSelectButton } from "@/components/account/chain-select-button";
 export { default as ViewSelectAccount } from "@/components/account/view-select-account";
 export { default as ViewSelectWallet } from "@/components/account/view-select-wallet";
