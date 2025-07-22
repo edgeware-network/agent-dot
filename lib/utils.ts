58c55da5f50e74c2219da7fd38e83602d7a1ea63
pranavcpawar <pranavcpawar@outlook.com>
2025-07-22 19:04:33 +0530

    feat(setup): configure drizzle and neon

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,6 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
+import { customAlphabet } from "nanoid";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -9,3 +10,5 @@ export function trimAddress(address: string, length?: number) {
   length ??= 4;
   return `${address.slice(0, length)}...${address.slice(-length)}`;
 }
+
+export const nanoid = customAlphabet("abcdefghijklmnopqrstuvwxyz0123456789");
