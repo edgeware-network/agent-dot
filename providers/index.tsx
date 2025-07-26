d403b54e72371fc261e389442c3239df47076a18
pranavcpawar <pranavcpawar@outlook.com>
2025-07-26 23:36:13 +0530

    refactor(providers): add default chain

--- providers/index.tsx
+++ providers/index.tsx
@@ -1,12 +1,15 @@
 "use client";
 
+import { chainConfig } from "@/papi-config";
 import { ExtensionProvider } from "@/providers/extension-provider";
 import { LightClientApiProvider } from "@/providers/light-client-provider";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <ExtensionProvider>
-      <LightClientApiProvider>{children}</LightClientApiProvider>
+      <LightClientApiProvider defaultChain={chainConfig[0]}>
+        {children}
+      </LightClientApiProvider>
     </ExtensionProvider>
   );
 }
