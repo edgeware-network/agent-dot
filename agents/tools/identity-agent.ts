1c8ce649d7df64b48aedc0926fc996a692a8adb7
pranavcpawar <pranavcpawar@outlook.com>
2025-07-28 00:09:18 +0530

    feat(agent): add get balance tool

---
+++ agents/tools/identity-agent.ts
@@ -0,0 +1,20 @@
+// client-side tools that are automatically executed on the client
+
+import { tool } from "ai";
+import z from "zod";
+
+const getBalances = tool({
+  name: "getBalances",
+  description:
+    "Read the on-chain balance of a given polkadot-compatible wallet address on a specific network/chain (switch network/chain first). If wallet address is not specified use active account. If network/chain is not specified use active network/chain.",
+  inputSchema: z.object({
+    address: z
+      .string()
+      .describe("A SS58-encoded wallet address to read the balance from."),
+    network: z
+      .string()
+      .describe("The name of the network/chain to read the balance from."),
+  }),
+});
+
+export { getBalances };
